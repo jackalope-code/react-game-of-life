cfd0bc46b48c2ab6e8d817e39918cd69f4d263c4
Sean <seanphelan97@gmail.com>
2021-05-17 00:31:52 -0600

    add controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { forwardRef, useState } from 'react';
+import React, {useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {FixedSizeGrid} from 'react-window';
 import styled from 'styled-components';
+import {debounce} from 'lodash';
 
 interface StateData {
   grid: boolean[][],
+  useMultiSelect: boolean,
   clickHandler: (e: React.MouseEvent<HTMLDivElement>, columnIndex: number, rowIndex: number) => void;
 }
 
@@ -48,7 +50,6 @@ const countNeighbors = (stateArray: boolean[][], x: number, y: number) => {
 }
 
 const advanceState = (prevState: boolean[][], width: number, height: number) => {
-  console.log("ADVANCE STATE DEBUG", prevState)
   // TODO: use a rectangle instead of a square
   let newState = createNewDataGrid(width, height, false)
   for(let i=0; i<width; i++) {
@@ -83,8 +84,16 @@ const CellRenderer = ({columnIndex, rowIndex, style, data}: CellRendererProps) =
   // } else {
   //   console.log('NO GRID FOUND', data)
   // }
+  let onMouseEnterHandler;
+  let onMouseDownHandler;
+  if(data.useMultiSelect) {
+    onMouseEnterHandler = (e: React.MouseEvent<HTMLDivElement>) => data.clickHandler(e, columnIndex, rowIndex)
+  } else {
+    onMouseDownHandler = (e: React.MouseEvent<HTMLDivElement>) => data.clickHandler(e, columnIndex, rowIndex)
+  }
+
   return (
-    <StyledCell style={style} onMouseDown={(e) => data.clickHandler(e, columnIndex, rowIndex)} isActive={data.grid[rowIndex][columnIndex]}>cell {columnIndex}-{rowIndex}</StyledCell>
+    <StyledCell style={style} onMouseEnter={onMouseEnterHandler} onMouseDown={onMouseDownHandler} isActive={data.grid[rowIndex][columnIndex]}></StyledCell>
   )
 }
 
@@ -95,24 +104,69 @@ function createNewDataGrid<DataType>(width: number, height: number, defaultValue
   });
   return grid;
 }
+let currentStartHandle: NodeJS.Timeout;
+
+const BigButton = styled.button`
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 2px;
+  margin: 10px;
+  background-color: #e9e9e9;
+  border: 2px solid black;
+  // display: inline-block;
+  font-size: 24px;
+`;
 
 function App() {
   const GRID_SIZE = 100;
   const [gridState, setGridState] = useState(createNewDataGrid<boolean>(GRID_SIZE, GRID_SIZE, false));
+  const [cellSliderValue, setCellSliderValue] = useState(40);
+  const [multiSelectMode, setMultiSelectMode] = useState(false);
+  const [frameRateSliderValue, setFrameRateSliderValue] = useState(4);
+  const [runningFlag, setRunningFlag] = useState(false);
 
   const step = () => {
     setGridState(prevState => advanceState(prevState, GRID_SIZE, GRID_SIZE));
   }
 
+  const start = () => {
+    if(currentStartHandle) {
+      clearInterval(currentStartHandle);
+    }
+    // Cap max FPS from input
+    step();
+    currentStartHandle = setInterval(step, Math.max(1000/frameRateSliderValue, 100));
+    setRunningFlag(true);
+  }
+
+  const stop = () => {
+    clearInterval(currentStartHandle);
+    setRunningFlag(false);
+  }
+
+  useEffect(() => {
+    stop();
+    start();
+  }, [frameRateSliderValue]);
+
   const handleOnClick = (e: React.MouseEvent<HTMLDivElement>, columnIndex: number, rowIndex: number) => {
     // setCellActive((prevState: boolean) => !prevState);
     console.log('CLICK EVENT', columnIndex, rowIndex)
-    setGridState(prevState => {
-      console.log('PREV STATE', prevState);
+    console.log(gridState[columnIndex][rowIndex]);
+    setGridState(prevState => immutableGridUpdate(prevState, columnIndex, rowIndex, !prevState[rowIndex][columnIndex]))
+
+  }
+
+  const handleMouseOver = (e: React.MouseEvent<HTMLDivElement>, columnIndex: number, rowIndex: number) => {
+    setGridState(prevState => immutableGridUpdate(prevState, columnIndex, rowIndex, true));
+  }
+
+  function immutableGridUpdate<T>(prevState: T[][], columnIndex: number, rowIndex: number, newValue: T): T[][] {
       const modifiedSlice = prevState[rowIndex].slice();
-      modifiedSlice[columnIndex] = !modifiedSlice[columnIndex];
+      modifiedSlice[columnIndex] = newValue;
       return [...prevState.slice(0, rowIndex), modifiedSlice, ...prevState.slice(rowIndex+1)]
-    })
   }
 
   interface ForwardProps {
@@ -141,17 +195,33 @@ function App() {
             // ></CellRenderer>
 
   // TODO: update to actually use current state to render properly
+  const eventHandler = multiSelectMode ? debounce(handleMouseOver, 10) : handleOnClick;
+
+  const slideEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFrameRateSliderValue(parseFloat(e.currentTarget.value))
+  }
+
+  const debouncedSlideEventHandler = debounce(slideEventHandler, 100);
+
   return (
     <div className="App">
-      <FixedSizeGrid columnWidth={60} rowHeight={60}
+      <FixedSizeGrid columnWidth={cellSliderValue} rowHeight={cellSliderValue}
         columnCount={GRID_SIZE} rowCount={GRID_SIZE}
-        itemData={{grid: gridState, clickHandler: handleOnClick}} width={1000} height={800}
+        itemData={{grid: gridState, useMultiSelect: multiSelectMode, clickHandler: eventHandler}} width={1000} height={800}
         // outerElementType={outerElementType}
       >
         {CellRenderer}
       </FixedSizeGrid>
       <div>
-        <button onClick={step}>Step</button>
+        <BigButton onClick={start}>Start</BigButton>
+        <BigButton onClick={stop}>Stop</BigButton>
+        <BigButton onClick={step}>Step</BigButton>
+        <label htmlFor="fps-slider">{frameRateSliderValue} FPS</label>
+        <input type="range" min="0.5" max="10" value={frameRateSliderValue} onChange={slideEventHandler} id="fps-slider" />
+        <label htmlFor="cell-slider">{cellSliderValue}px</label>
+        <input type="range" min="15" max="100" value={cellSliderValue} onChange={(e) => setCellSliderValue(parseInt(e.currentTarget.value))} id="cell-slider"/>
+        <label htmlFor="mouse-select-mode">Multi select mode</label>
+        <input type="checkbox" defaultChecked={multiSelectMode} onChange={() => setMultiSelectMode(!multiSelectMode)} id="mouse-select-mode"></input>
       </div>
     </div>
   );
